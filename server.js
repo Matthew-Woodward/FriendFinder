b343433b0483a4283e53fb7f200a011c856b9d1a
matthew woodward <matthew.woodward1982@gmail.com>
2019-01-31 19:37:07 -0700

    final

---
+++ server.js
@@ -0,0 +1,16 @@
+let express = require("express");
+let path = require("path");
+
+let app = express();
+let PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function () {
+    console.log("App listening on http://localhost:" + PORT);
+});
+
+require('./app/routing/htmlRoutes.js')(app);
+
+require('./app/routing/apiRoutes.js')(app);
